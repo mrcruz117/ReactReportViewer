fb0fa589108a4e784fcab78fe335f4f724b4b520
Michael Cruz <mcruz@aersale.com>
2021-11-01 10:30:47 -0400

    rendering demo report

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 /* eslint-disable */
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 //Report Viewer source
-import '@boldreports/javascript-reporting-controls/Scripts/bold.report-viewer.min';
-import '@boldreports/javascript-reporting-controls/Content/material/bold.reports.all.min.css';
+import "@boldreports/javascript-reporting-controls/Scripts/bold.report-viewer.min";
+import "@boldreports/javascript-reporting-controls/Content/material/bold.reports.all.min.css";
 //Data-Visualization
-import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.bulletgraph.min';
-import '@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.chart.min';
+import "@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.bulletgraph.min";
+import "@boldreports/javascript-reporting-controls/Scripts/data-visualization/ej.chart.min";
 //Reports react base
-import '@boldreports/react-reporting-components/Scripts/bold.reports.react.min';
+import "@boldreports/react-reporting-components/Scripts/bold.reports.react.min";
 
-var viewerStyle = {'height': '700px', 'width': '100%'};
+var viewerStyle = { height: "700px", width: "100%" };
 
 function App() {
   return (
     <div style={viewerStyle}>
       <BoldReportViewerComponent
-      id="reportviewer-container">
-      </BoldReportViewerComponent>
+        id="reportviewer-container"
+        reportServiceUrl={
+          "https://demos.boldreports.com/services/api/ReportViewer"
+        }
+        reportPath={"~/Resources/docs/sales-order-detail.rdl"}
+      ></BoldReportViewerComponent>
     </div>
   );
 }
